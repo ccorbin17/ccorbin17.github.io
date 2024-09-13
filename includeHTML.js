1b4cbf500690521922dcd6114834eb4a4b9ce981
Carina <carinabcorbin@gmail.com>
2024-09-13 11:39:58 -0400

    add javascript to dynamically add the header to each page

---
+++ includeHTML.js
@@ -0,0 +1,28 @@
+function includeHTML() {
+    const z = document.getElementsByTagName("*");
+    for (let i = 0; i < z.length; i++) {
+        const elmnt = z[i];
+        const file = elmnt.getAttribute("include-html");
+        if (file) {
+            fetch(file)
+                .then(response => response.text())
+                .then(data => elmnt.innerHTML = data)
+                .catch(error => console.log('Error:', error));
+            return;
+        }
+    }
+}
+
+function includeHeadElements() {
+    const headContent = `
+        <title>Carina's Corner</title>
+        <link rel="shortcut icon" type="image/x-icon" href="me.jpg" />
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
+    `;
+    document.head.insertAdjacentHTML('beforeend', headContent);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    includeHTML();
+    includeHeadElements();
+});
